501ec7d0b94836ef3e70d8afcb7bfada153763e6
Zubin Pratap <zubin.pratap@gmail.com>
2023-02-22 15:18:48 -0800

    INTERIM--- working on stablecoin payments

---
+++ tasks/Sample-apps-tasks/deploySTC.js
@@ -0,0 +1,51 @@
+const { types } = require("hardhat/config")
+const { VERIFICATION_BLOCK_CONFIRMATIONS, networkConfig } = require("../../network-config")
+
+task("samples-deploy-stablecoin", "Deploys the StableCoin contract for Twilio-Spotify sample")
+  .addOptionalParam("verify", "Set to true to verify client contract", false, types.boolean)
+  .setAction(async (taskArgs) => {
+    if (network.name === "hardhat") {
+      throw Error(
+        'This command cannot be used on a local hardhat chain.  Specify a valid network or simulate an FunctionsConsumer request locally with "npx hardhat functions-simulate".'
+      )
+    }
+
+    console.log(`Deploying StableCoin contract to ${network.name}`)
+
+    console.log("\n__Compiling Contracts__")
+    await run("compile")
+
+    const stcContractFactory = await ethers.getContractFactory("StableCoin")
+
+    const stcContract = await stcContractFactory.deploy()
+
+    console.log(
+      `\nWaiting ${VERIFICATION_BLOCK_CONFIRMATIONS} blocks for transaction ${stcContract.deployTransaction.hash} to be confirmed...`
+    )
+    await stcContract.deployTransaction.wait(VERIFICATION_BLOCK_CONFIRMATIONS)
+
+    const verifyContract = taskArgs.verify
+
+    if (verifyContract && (process.env.POLYGONSCAN_API_KEY || process.env.ETHERSCAN_API_KEY)) {
+      try {
+        console.log("\nVerifying contract...")
+        await stcContract.deployTransaction.wait(Math.max(6 - VERIFICATION_BLOCK_CONFIRMATIONS, 0))
+        await run("verify:verify", {
+          address: stcContract.address,
+          constructorArguments: [],
+        })
+        console.log("Contract verified")
+      } catch (error) {
+        if (!error.message.includes("Already Verified")) {
+          console.log("Error verifying contract.  Try delete the ./build folder and try again.")
+          console.log(error)
+        } else {
+          console.log("Contract already verified")
+        }
+      }
+    } else if (verifyContract) {
+      console.log("\nPOLYGONSCAN_API_KEY or ETHERSCAN_API_KEY missing. Skipping contract verification...")
+    }
+
+    console.log(`\tStableCCoin contract deployed to ${stcContract.address} on ${network.name}`)
+  })
---
+++ tasks/Sample-apps-tasks/index.js
@@ -0,0 +1,1 @@
+exports.deploySTC = require("./deploySTC")
